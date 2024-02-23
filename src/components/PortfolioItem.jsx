60d9f090784cec4c59cbc28867bba03ff4ce4a85
kdeoAxelor <k.deo@axelor.com>
2024-02-23 16:06:23 +0530

    portfolio page done

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,45 +1,47 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Close from "../assets/close.svg";
 
 const PortfolioItem = ({ img, title, details }) => {
-  const [model,setModel] = useState(false);
+  const [model, setModel] = useState(false);
 
-  const toggleModel=()=>{
+  const toggleModel = () => {
     setModel(!model);
-  }
+  };
   return (
-    <div>
+    <>
       <div className="portfolio__item">
         <img src={img} alt="" className="portfolio__img" />
+
         <div className="portfolio__hover" onClick={toggleModel}>
           <h3 className="portfolio__title">{title}</h3>
         </div>
+        
         {model && (
           <div className="portfolio__model">
-          <div className="portfolio-model-content">
-            <img src={Close} alt="model" className="model__close" />
-            <h3 className="model__title">{title}</h3>
+            <div className="portfolio-model-content">
+              <img src={Close} alt="model" className="model__close" onClick={toggleModel}/>
+              <h3 className="model__title">{title}</h3>
 
-            <ul className="model_list grid">
-              {details.map(({ icon, title, desc }, index) => {
-                return (
-                  <div key={index} className="model__item">
-                    <span className="item__icon">{icon}</span>
-                    <div>
-                      <span className="item__title">{title}</span>
-                      <span className="item__desc">{desc}</span>
-                    </div>
-                  </div>
-                );
-              })}
-            </ul>
+              <ul className="model_list grid">
+                {details.map(({ icon, title, desc }, index) => {
+                  return (
+                    <li key={index} className="model__item">
+                      <span className="item__icon">{icon}</span>
+                      <div>
+                        <span className="item__title">{title}</span>
+                        <span className="item__details">{desc}</span>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
 
-            <img src={img} className="model__img" />
+              <img src={img} className="model__img" />
+            </div>
           </div>
-        </div>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
